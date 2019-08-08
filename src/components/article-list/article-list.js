40b55e640dee8fc10d2c7aee5fd4d36be82d10a5
Eugene Ugrevatov <e.ugrevatov@gmail.com>
2019-08-08 23:24:01 +0300

    acticle list -> functional component

--- src/components/article-list/article-list.js
+++ src/components/article-list/article-list.js
@@ -3,27 +3,22 @@ import { connect } from "react-redux";
 import Article from "../article";
 import accordion from "../../decorators/accordion";
 
-@connect(state => ({ articles: filterArticles(state) }))
-@accordion
-class ArticleList extends React.Component {
-  render() {
-    const { articles, defaultOpenId, isOpen, setOpenId } = this.props;
+function ArticleList({ articles, isOpen, setOpenId }) {
+  const setContainerRef = containerRef =>
+    console.log("container: ", containerRef);
 
-    return (
-      <div ref={this.setContainerRef}>
-        {articles.map(article => (
-          <Article
-            key={article.id}
-            article={article}
-            isOpen={isOpen(article.id)}
-            onBtnClick={setOpenId(article.id)}
-          />
-        ))}
-      </div>
-    );
-  }
-
-  setContainerRef = containerRef => console.log("container: ", containerRef);
+  return (
+    <div ref={setContainerRef}>
+      {articles.map(article => (
+        <Article
+          key={article.id}
+          article={article}
+          isOpen={isOpen(article.id)}
+          onBtnClick={setOpenId(article.id)}
+        />
+      ))}
+    </div>
+  );
 }
 
 function filterArticles(state) {
@@ -37,32 +32,6 @@ function filterArticles(state) {
     );
 }
 
-/*
-class ArticleList extends Component {
-  state = {
-    openArticleId: this.props.defaultOpenId
-  };
-
-  render() {
-    if (this.state.error) {
-      return <h3>Some error</h3>;
-    }
-    return (
-      <div>
-        {this.props.articles.map(article => (
-          <Article
-            key={article.id}
-            article={article}
-            isOpen={article.id === this.state.openArticleId}
-            onBtnClick={this.toggleOpenArticle(article.id)}
-          />
-        ))}
-      </div>
-    );
-  }
-
-  toggleOpenArticle = openArticleId => () => this.setState({ openArticleId });
-}
-*/
-
-export default ArticleList;
+export default connect(state => ({ articles: filterArticles(state) }))(
+  accordion(ArticleList)
+);
